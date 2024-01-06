b487dacf777288e2d72ee2bced9d30af5c6cc470
MBABAZI Jacques <71966425+devcron33886@users.noreply.github.com>
2024-01-06 12:47:04 +0200

    updated branding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,13 +10,13 @@ class DatabaseSeeder extends Seeder {
 	 * Seed the application's database.
 	 */
 	public function run(): void {
-		/*$this->call([
+		$this->call([
 			UserSeeder::class,
 			TeamSeeder::class,
 			CategorySeeder::class,
 			CountrySeeder::class,
-		]);*/
-		//\App\Models\Player::factory(6000)->create();
+		]);
+		\App\Models\Player::factory(6000)->create();
 		\App\Models\Performance::factory(24000)->create();
 
 	}
